5b0d67537f8d9d9170202f6eaf81bb9933f89ecb
AkshayEddula <akshayddula454@gmail.com>
2025-06-03 11:38:36 +0530

    added fixes

--- app/checkout/route.ts
+++ app/checkout/route.ts
@@ -4,6 +4,6 @@ import { Checkout } from "@polar-sh/nextjs";
 export const GET = Checkout({
     accessToken: process.env.POLAR_ACCESS_TOKEN,
     successUrl: "https://www.notesnap.app/checkout/success",
-    server: "sandbox",
+    server: "production",
     // Use sandbox if you're testing Polar - omit the parameter or pass 'production' otherwise
 });
